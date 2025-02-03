0fdb31d853e847810a06a66bc53cc8b777268a52
NatanVilasBoasAGL <natan.boas@aglsolutions.com.br>
2025-02-03 10:09:20 -0300

    adding background, configure files and babel

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["bable-preset-expo"],
+    presets: ["babel-preset-expo"],
     plugins: ["react-native-reanimated/plugin"],
   };
 };
---
+++ components/HomeBackground.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Canvas, LinearGradient, Rect, vec } from "@shopify/react-native-skia";
+import {
+  Image,
+  ImageBackground,
+  StyleSheet,
+  useWindowDimensions,
+} from "react-native";
+const HomeBackground = () => {
+  const { width, height } = useWindowDimensions();
+  return (
+    <>
+      <Canvas style={{ flex: 1 }}>
+        <Rect x={0} y={0} width={width} height={height}>
+          <LinearGradient
+            start={vec(0, 0)}
+            end={vec(width, height)}
+            colors={["#2E335A", "#1C1B33"]}
+          />
+        </Rect>
+      </Canvas>
+      <ImageBackground
+        source={require("../assets/home/Background.png")}
+        resizeMode="cover"
+        style={{ height: "100%" }}
+      >
+        <Image
+          source={require("../assets/home/House.png")}
+          resizeMode="cover"
+          style={{
+            width: width,
+            height: width,
+            ...StyleSheet.absoluteFillObject,
+            top: 304,
+          }}
+        />
+      </ImageBackground>
+    </>
+  );
+};
+
+export default HomeBackground;
