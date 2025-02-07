da8998397b55e5061575221c081b4ca3df831d4f
NatanVilasBoasAGL <natan.boas@aglsolutions.com.br>
2025-02-07 17:06:55 -0300

    create tab icons

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import HomeBackground from "./components/HomeBackground";
 import { StatusBar } from "expo-status-bar";
+import { WeatherTabBar } from "./components/tabbar/WeatherTabBar";
 
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
       <HomeBackground />
+      <WeatherTabBar />
     </>
   );
 }
--- components/tabbar/WeatherTabBar.tsx
+++ components/tabbar/WeatherTabBar.tsx
@@ -1,10 +1,22 @@
-import { View } from "react-native";
-import { ArcComponent } from "./elements/ArcComponent";
+import { StyleSheet, View } from "react-native";
+import ArcComponent from "./elements/ArcComponent";
+import useApplicationDimensions from "../../hooks/useApplicationDimensions";
+import TabbarItems from "./elements/TabbarItems";
 
 export const WeatherTabBar = () => {
+  const TabbarHeight = 88;
+  const { width, height } = useApplicationDimensions();
+
   return (
-    <View style={{ flex: 1, backgroundColor: "red" }}>
-      <ArcComponent />
+    <View
+      style={{
+        height: TabbarHeight,
+        ...StyleSheet.absoluteFillObject,
+        top: height - TabbarHeight,
+      }}
+    >
+      <ArcComponent height={TabbarHeight} width={width} />
+      <TabbarItems />
     </View>
   );
 };
