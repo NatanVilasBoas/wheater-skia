c94e2756797ae2ab866998d01d89de55927ce550
NatanVilasBoasAGL <natan.boas@aglsolutions.com.br>
2025-02-10 14:45:20 -0300

    create add button

--- components/tabbar/elements/TabbarItems.tsx
+++ components/tabbar/elements/TabbarItems.tsx
@@ -1,9 +1,16 @@
-import { View } from "react-native";
+import { Pressable, StyleSheet, View } from "react-native";
 import ListIcon from "../icons/ListIcon";
 import MapIcon from "../icons/MapIcon";
 import TrapezoidBackground from "./TrapezoidBackground";
+import useApplicationDimensions from "../../../hooks/useApplicationDimensions";
+import CircleButton from "./CricleButton";
 
 const TabbarItems = () => {
+  const { width, height } = useApplicationDimensions();
+  const trapezoidWidth = width * 0.68;
+  const trapezoidHeight = height * 0.12;
+  const circleRadius = (trapezoidHeight * 0.51) / 2;
+  const buttonCenterX = width / 2 - circleRadius;
   return (
     <View
       style={{
@@ -15,7 +22,20 @@ const TabbarItems = () => {
       }}
     >
       <MapIcon />
-      <TrapezoidBackground />
+      <TrapezoidBackground width={trapezoidWidth} height={trapezoidHeight} />
+      <Pressable
+        style={{
+          ...StyleSheet.absoluteFillObject,
+          left: buttonCenterX,
+          top: 12,
+          width: circleRadius * 2,
+          height: circleRadius * 2,
+        }}
+      >
+        {({ pressed }) => (
+          <CircleButton radius={circleRadius} pressed={pressed} />
+        )}
+      </Pressable>
       <ListIcon />
     </View>
   );
