782f35d0aa25d432be92dd800c8c4891ebff62cf
NatanVilasBoasAGL <natan.boas@aglsolutions.com.br>
2025-02-18 08:53:36 -0300

    create forecast capsules

--- components/sheet/ForecastSheet.tsx
+++ components/sheet/ForecastSheet.tsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
 import ForecastSheetBackground from "./ForecastSheetBackground";
 import useApplicationDimensions from "../../hooks/useApplicationDimensions";
 import ForecastControl from "./elements/ForecastControl";
 import Separator from "./elements/Separator";
+import { hourly, weekly } from "../../data/ForecastData";
+import ForecastScroll from "../forecast/ForecastScroll";
+import { ForecastType } from "../../models/Weather";
 
 export default function ForecastSheet() {
   const { width, height } = useApplicationDimensions();
+  const [selectedForecastType, setSelectedForecastType] =
+    useState<ForecastType>(ForecastType.Hourly);
   const snapPoints = ["38.5%", "83%"];
   const firstSnaPoint = height * (parseFloat(snapPoints[0]) / 100);
   const cornerRadius = 44;
+  const capsuleRadius = 30;
+  const capsuleHeight = height * 0.17;
+  const capsuleWidth = width * 0.15;
 
   return (
     <BottomSheet
@@ -28,8 +36,16 @@ export default function ForecastSheet() {
       )}
     >
       <BottomSheetView style={{ flex: 1, height: height }}>
-        <ForecastControl />
+        <ForecastControl onPress={(type) => setSelectedForecastType(type)} />
         <Separator width={width} height={3} />
+        <ForecastScroll
+          capsuleWidth={capsuleWidth}
+          capsuleHeight={capsuleHeight}
+          capsuleRadius={capsuleRadius}
+          forecasts={
+            selectedForecastType === ForecastType.Hourly ? hourly : weekly
+          }
+        />
       </BottomSheetView>
     </BottomSheet>
   );
